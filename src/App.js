bcc8e2f2cf2ab2e3a49005810fb602ae8607fbf3
Josiane Dufour <josiane@Josianes-MacBook-Pro.local>
2019-04-22 15:04:45 +0100

    implement routing on title to return to home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import { Router } from "@reach/router";
+import { Router, Link } from "@reach/router";
 import Results from "./Results";
 import Details from "./Details";
 
@@ -8,7 +8,10 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1> Adopt a Doggo </h1>
+        <header>
+          <Link to="/">Adopt a Doggo </Link>
+        </header>
+
         <Router>
           <Results path="/" />
           <Details path="/details/:id" />
