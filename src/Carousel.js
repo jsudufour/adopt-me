d3db70e59ed895bea4ae8ddb1302195efd2b37cf
Josiane Dufour <josiane@Josianes-MacBook-Pro.local>
2019-05-07 00:13:18 +0100

    bind this to correct context by changing handleClick event handler to arrow function, thus removing its own scope

--- src/Carousel.js
+++ src/Carousel.js
@@ -16,18 +16,25 @@ class Carousel extends React.Component {
     return { photos };
   }
 
+  handleIndexClick = event => {
+    this.setState({
+      active: +event.target.dataset.index
+    });
+  };
+
   render() {
     const { photos, active } = this.state;
-
-    console.log(photos);
 
     return (
       <div className="carousel">
         <img src={photos[active]} alt="primary animal" />
         <div className="carousel-smaller">
           {photos.map((photo, index) => (
+            /* eslint-disable-next-line */
             <img
+              onClick={this.handleIndexClick}
               key={photo}
+              data-index={index}
               src={photo}
               className={index === active ? "active" : ""}
               alt="animal thumbnail"
